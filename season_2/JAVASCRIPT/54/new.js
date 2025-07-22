52b7caa252e601998ee50ad613d112e55eddedd9
sumit vikram <vikramsumit@outlook.com>
2025-07-22 22:45:51 +0530

    Initial commit

---
+++ season_2/JAVASCRIPT/54/new.js
@@ -0,0 +1,3 @@
+console.log("Hello world");
+console.log("This is a log message....");
+console.info("This is an info message....");
---
+++ season_2/JAVASCRIPT/54/script.js
@@ -0,0 +1,20 @@
+alert("Hello world")
+
+console.log("Hello world");
+console.log("This is a log message");
+console.info("This is an info message");
+
+var a = prompt("Enter a value for 'a':");
+console.log("You entered: " + a);
+var isTrue = confirm("Are you sure?, you want to leave this page?");
+if (isTrue) {
+    console.log("You confirmed the action.");
+} else {
+    console.log("You cancelled the action.");
+}   
+// console.log("You entered: " + isTrue);
+
+
+
+// console.error("This is an error message");
+// console.warn("This is a warning message");
