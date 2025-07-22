b5ac6e4ef866660938d2710f5be0935c213dcaa2
sumit vikram <vikramsumit@outlook.com>
2025-07-23 00:41:54 +0530

    Initial commit

---
+++ season_2/JAVASCRIPT/55/script.js
@@ -0,0 +1,12 @@
+console.log("Hey this is a test script for JavaScript!");
+
+var a = 5;
+var b = 10;
+var c = "Raju";
+var sum = a + b;
+console.log("The sum of " + a + " and " + b + " is: " + sum);
+
+console.log(typeof a, typeof b, typeof c);
+console.log("The type of 'a' is: " + typeof a);
+console.log("The type of 'b' is: " + typeof b);
+console.log("The type of 'c' is: " + typeof c); 
