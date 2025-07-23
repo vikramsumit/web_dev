c3b14a0f6088c00dcc656931aa2c22ea3c4b8298
sumit vikram <vikramsumit@outlook.com>
2025-07-23 23:27:13 +0530

    Initial commit

--- season_2/JAVASCRIPT/55/script.js
+++ season_2/JAVASCRIPT/55/script.js
@@ -1,6 +1,6 @@
 console.log("Hey this is a test script for JavaScript!");
 
-var a = 5;
+let a = 5;
 var b = 10;
 var c = "Raju";
 var sum = a + b;
@@ -10,3 +10,31 @@ console.log(typeof a, typeof b, typeof c);
 console.log("The type of 'a' is: " + typeof a);
 console.log("The type of 'b' is: " + typeof b);
 console.log("The type of 'c' is: " + typeof c); 
+
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+let x = "Raju bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "Raju",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
