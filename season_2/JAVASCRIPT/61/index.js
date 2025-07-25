1529cbfa8947bedc95f83cd946638a2a625790b9
sumit vikram <vikramsumit@outlook.com>
2025-07-25 15:40:06 +0530

    Initial commit

--- season_2/JAVASCRIPT/61/index.js
+++ season_2/JAVASCRIPT/61/index.js
@@ -1,3 +1,5 @@
+import { question } from 'readline-sync';
+
 let random = Math.random()
 // console.log(random)
 // let a = prompt("Enter first number")
@@ -5,9 +7,9 @@ let random = Math.random()
 // let b = prompt("Enter second number")
 
 // Use readline.question for input
-let a = readline.question("Enter first number: "); 
-let c = readline.question("Enter operation (+,-,*,/): ");
-let b = readline.question("Enter second number: ");
+let a = question("Enter first number: "); 
+let c = question("Enter operation (+,-,*,/): ");
+let b = question("Enter second number: ");
 
 let obj = {
     "+": "-",
@@ -16,14 +18,15 @@ let obj = {
     "/": "**",
 }
 
-if (random > 0.1) {
+if (random > 0.9) {
     console.log(`The result is ${a} ${c} ${b}`)
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    // console.log(`The result is ${eval(`${a} ${c} ${b}`)}`)
     
 }
 
 else {
     c = obj[c]
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
+    console.log(`The result is ${eval(`${a} ${c} ${b}`)}`) 
 
 }
+
