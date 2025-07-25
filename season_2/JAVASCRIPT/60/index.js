adef5b74011a6c712a1d0f90497317d0a24f68e9
sumit vikram <vikramsumit@outlook.com>
2025-07-25 15:27:56 +0530

    Initial commit

---
+++ season_2/JAVASCRIPT/60/index.js
@@ -0,0 +1,28 @@
+console.log('This is string in 60/index.js');
+let a = "raju";
+console.log(a[0]); 
+console.log(a[1]); 
+console.log(a[2]); 
+console.log(a[3]); 
+console.log(a[4]); 
+console.log(a.length); 
+// console.log(a.charAt(0)); 
+// console.log(a.charAt(1)); 
+// console.log(a.charAt(2)); 
+// console.log(a.charAt(3)); 
+// console.log(a.charAt(4)); 
+
+let _name = "raju";
+let friend = "suresh";
+console.log(_name + " is a friend of " + friend);
+console.log(`${_name} is a friend of ${friend}`);
+
+let b = "karthik";
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("ka", "77"))
+console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
