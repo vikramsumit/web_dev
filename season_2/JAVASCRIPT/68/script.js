f74747171f87252847726ba7271b62d29db53214
sumit vikram <vikramsumit@outlook.com>
2025-07-29 18:46:05 +0530

    Initial commit

---
+++ season_2/JAVASCRIPT/68/script.js
@@ -0,0 +1,15 @@
+console.log("Raju")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+document.getElementById("redbox").style.backgroundColor = "blue"
+
+document.querySelector(".box").style.backgroundColor = "green";
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// }) 
