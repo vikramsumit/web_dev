18e18290d8f129ec61b7384dc19ee5a65343fa5f
sumit vikram <vikramsumit@outlook.com>
2025-07-31 15:24:34 +0530

    Initial commit

---
+++ season_2/JAVASCRIPT/74/script.js
@@ -0,0 +1,32 @@
+let button = document.getElementById('myButton');
+
+
+button.addEventListener('click', ()=> {
+    document.querySelector(".box").innerHTML = 'Button Clicked!';
+    // alert('Button was clicked!');
+});
+
+button.addEventListener('contextmenu', ()=> {
+    // document.querySelector(".box").innerHTML = 'Button Clicked!';
+    alert('Dont right click me!')
+});
+
+document.addEventListener('keydown', (e)=> {
+    console.log(e.key,e.keyCode);
+    if (e.key === 'Enter') {
+        document.querySelector(".box").innerHTML = 'Enter Key Pressed!';
+    }
+    if (e.key === 'Escape') {
+        document.querySelector(".box").innerHTML = 'Escape Key Pressed!';
+    }
+    if (e.key === 'ArrowUp') {
+        document.querySelector(".box").innerHTML = 'Arrow Up Key Pressed!';
+    }
+    if (e.key === 'ArrowDown') {
+        document.querySelector(".box").innerHTML = 'Arrow Down Key Pressed!';
+    }
+    if (e.key === 'ArrowLeft') {
+        document.querySelector(".box").innerHTML = 'Arrow Left Key Pressed!';
+    }
+    
+})
