d432b506e5f99fa2836c8695fe078a4f767c9679
sumit vikram <vikramsumit@outlook.com>
2025-09-04 23:59:24 +0530

    Initial commit

---
+++ season_2/JAVASCRIPT/89/main.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//     console.log("Hey this is a get request")
+//     res.send('Hello World!')
+// })
+
+// app.post('/', (req, res) => {
+//     console.log("Hey this is a post request")
+//     res.send('Hello World from post req!')
+// })
+
+// app.put('/', (req, res) => {
+//     console.log("Hey this is a put request")
+//     res.send('Hello World from put req!')
+// })
+
+// app.delete('/', (req, res) => {
+//     console.log("Hey this is a delete request")
+//     res.send('Hello World from delete req!')
+// })
+
+// Chaining of requests
+app.get('/', (req, res) => {
+    console.log("Hey this is a get request")
+    res.send('Hello World!')
+}).post('/', (req, res) => {
+    console.log("Hey this is a post request")
+    res.send('Hello World from post req!')
+}).put('/', (req, res) => {
+    console.log("Hey this is a put request")
+    res.send('Hello World from put req!')
+}).delete('/', (req, res) => {
+    console.log("Hey this is a delete request")
+    res.send('Hello World from delete req!')
+})
+
+app.get("/index", (req, res) => {
+    console.log("Hey its index")
+    // res.send("This is index page")
+    res.sendFile('templates/1.html', { root: __dirname })
+})
+
+app.get("/api", (req, res) => {
+    res.json({ name: "Raju", job: "Developer", age: 24 , hobbies: ['coding', 'sleeping', 'listening music']})
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
